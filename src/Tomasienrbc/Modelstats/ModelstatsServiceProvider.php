ca4b5c5702b99c1aea5e10b93a238996c31dbee1
Tommy Nicholas <tomasienrbc@gmail.com>
2015-12-07 17:43:08 -0500

    Minor changes that maybe will help?? HALP???!!!!
    HALP!

--- src/Tomasienrbc/Modelstats/ModelstatsServiceProvider.php
+++ src/Tomasienrbc/Modelstats/ModelstatsServiceProvider.php
@@ -16,32 +16,39 @@ class ModelstatsServiceProvider extends ServiceProvider {
 	 *
 	 * @return void
 	 */
+
+
+	 /**
+ 	 * Register the service provider.
+ 	 *
+ 	 * @return void
+ 	 */
+ 	 public function register()
+ 	 {
+
+		 $this->app->bind('statspage',function($app) {
+			 return new Statspage;
+		 });
+
+ 	// 	 $this->app['statspage'] = $this->app->share(function($app)
+ 	// 	 {
+ 	// 			 return new Statspage;
+ 	// 	 });
+
+ 		 // Shortcut so developers don't need to add an Alias in app/config/app.php
+ 		 $this->app->booting(function()
+ 		 {
+ 				 $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+ 				 $loader->alias('UnderlyingClass', 'Tomasienrbc\Modelstats\Facades\Statspage');
+ 		 });
+ 	 }
+
 	public function boot()
 	{
 		$this->package('tomasienrbc/modelstats','statspage');
 		$this->loadViewsFrom(__DIR__.'/views', 'statspage');
 	}
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	 public function register()
-	 {
-
-		 $this->app['statspage'] = $this->app->share(function($app)
-		 {
-				 return new Statspage;
-		 });
-
-		 // Shortcut so developers don't need to add an Alias in app/config/app.php
-		 $this->app->booting(function()
-		 {
-				 $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-				 $loader->alias('UnderlyingClass', 'Tomasienrbc\Modelstats\Facades\Statspage');
-		 });
-	 }
 
 	/**
 	 * Get the services provided by the provider.
