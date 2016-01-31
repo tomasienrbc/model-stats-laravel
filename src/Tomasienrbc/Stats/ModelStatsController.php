29715892539e5c2d625ba82475912ff5fd9d0ed9
Tommy Nicholas <tomasienrbc@gmail.com>
2016-01-31 12:51:37 -0500

    Clean up + adding support for Laravel 5

--- src/Tomasienrbc/Stats/ModelStatsController.php
+++ src/Tomasienrbc/Stats/ModelStatsController.php
@@ -9,12 +9,13 @@ use \DateTime;
 
 class ModelStatsController extends BaseController {
 
+	// get Model Names for AJAX Call to front end on initial load
 	public function models() {
-		Log::info("SHIP IT!");
 		$models = $this->getModelNames();
 		return $models;
 	}
 
+  // routing function for taking in Inputs and returning model stats
 	public function model_stats() {
 
 		$models = Input::get("models");
@@ -23,15 +24,9 @@ class ModelStatsController extends BaseController {
 		$time_type = "".Input::get("time_type")."_at";
 		$return = $this->getModelCounts($models,$time_type,$start,$end);
 		return $return;
-
-		// array of tables in the database - OLD
-		// $all_tables = DB::select('SHOW TABLES');
-		// $tables = array();
-		// foreach ($all_tables as $key => $value) {
-		// 	array_push($tables,reset($value));
-		// }
 	}
 
+  // for any given model in any given time period, return the grouped counts by day
 	private function getModelCounts($model,$time_type,$start,$end) {
 		// get list of Model names
 		$return = array();
@@ -40,18 +35,6 @@ class ModelStatsController extends BaseController {
 		$count = $this->countModelsByTime($time_type,$model,$start,$end);
 		$return[$model] = $count;
 		return $return;
-
-		// old logic for doing every model which is bad
-		// if($models) {
-		// 	foreach ($models as $model) {
-		// 		$count = $this->countModelsByTime($time_type,$model);
-		// 		$return[$model] = $count;
-		// 	}
-		// 	return $return;
-		// } else {
-		// 	Log::info("sorry");
-		// 	return "sorry";
-		// }
 	}
 
 	// get list of Model names
@@ -77,6 +60,7 @@ class ModelStatsController extends BaseController {
 
 	// get count by day based on time attribute such as "created_at" or "updated_at"
 	private function countModelsByTime($attr,$model_raw,$first,$last) {
+		
 		$model = "\\".$model_raw;
 		date_default_timezone_set('America/New_York');
 		$today = date('Y-m-d');
