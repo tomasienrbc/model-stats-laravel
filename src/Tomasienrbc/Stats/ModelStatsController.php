2a1c151c02d10a8c09c62b79c8b7441a8152e39e
Tommy Nicholas <tomasienrbc@gmail.com>
2016-04-17 13:19:58 -0400

    Some improvements to the code and edge cases

--- src/Tomasienrbc/Stats/ModelStatsController.php
+++ src/Tomasienrbc/Stats/ModelStatsController.php
@@ -17,36 +17,26 @@ class ModelStatsController extends BaseController {
 
   // routing function for taking in Inputs and returning model stats
 	public function model_stats() {
-
 		$models = Input::get("models");
 		$start = Input::get("date_range_start");
 		$end = Input::get("date_range_end");
+		// you probably don't need the pre-string - Peter
 		$time_type = "".Input::get("time_type")."_at";
-		$return = $this->getModelCounts($models,$time_type,$start,$end);
-		return $return;
-	}
-
-  // for any given model in any given time period, return the grouped counts by day
-	private function getModelCounts($model,$time_type,$start,$end) {
-		// get list of Model names
-		$return = array();
-
-		// if we got models, do things. Else, kill, eventually try again
-		$count = $this->countModelsByTime($time_type,$model,$start,$end);
-		$return[$model] = $count;
+		$return = $this->getModelCounts($models, $time_type, $start, $end);
 		return $return;
 	}
 
 	// get list of Model names
 	private function getModelNames() {
+		/// Open the actual file and grep for class to be more precise
 		$path = app_path() . "/models";
-		$out=array();
-		if(file_exists($path)) {
+		$out = array();
+		if (file_exists($path)) {
 			$results = scandir($path);
-			foreach($results as $result) {
-				if($result === '.' or $result === '..') continue;
+			foreach ($results as $result) {
+				if ($result === '.' or $result === '..'  or $result[0] === '.') continue;
 				$filename = $result;
-				if(is_dir($filename)) {
+				if (is_dir($filename)) {
 					$out = array_merge($out, getModelNames($fileName));
 				} else {
 					$out[] = substr($filename,0,-4);
@@ -58,14 +48,27 @@ class ModelStatsController extends BaseController {
 		}
 	}
 
+	// for any given model in any given time period, return the grouped counts by day
+	private function getModelCounts($model,$time_type,$start,$end) {
+		// get list of Model names
+		$response = array();
+
+		// if we got models, do things. Else, kill, eventually try again
+		$count = $this->countModelsByTime($model,$time_type,$start,$end);
+		$response[$model] = $count;
+		return $response;
+	}
+
 	// get count by day based on time attribute such as "created_at" or "updated_at"
-	private function countModelsByTime($attr,$model_raw,$first,$last) {
-		
+	private function countModelsByTime($model_raw,$attr,$first,$last) {
+
 		$model = "\\".$model_raw;
+		/// move to get model counts or top of the file
 		date_default_timezone_set('America/New_York');
 		$today = date('Y-m-d');
 		// Get the information requested
 		// AFAIK you have to write an IF statement to figure out if we want created or updated at, because the string $attr can't be used as a constant
+		///String($attr)????
 		if($attr == "created_at") {
 			$days_fetch = $model::select($attr)
 					->whereBetween('created_at', array(new DateTime($first), new DateTime($last)))
