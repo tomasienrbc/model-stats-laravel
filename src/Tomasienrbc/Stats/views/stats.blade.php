3106e31ef432973cac83d26a512fb9d937e5dc84
Tommy Nicholas <tomasienrbc@gmail.com>
2016-12-10 12:07:10 -0500

    Re-designed view
    JS and CSS files still will only work on Blank Slate though tbh

--- src/Tomasienrbc/Stats/views/stats.blade.php
+++ src/Tomasienrbc/Stats/views/stats.blade.php
@@ -1,89 +1,192 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Blank Slate</title>
-    <meta name="description" content="Model Stats Page">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-<!--     <meta name="apple-mobile-web-app-capable" content="yes"> -->
-    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <link rel="icon" href="/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="../css/plottable.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/qtip2/2.2.1/jquery.qtip.min.css">
-    <link rel="stylesheet" href="../css/stats.css">
+  <title>Model Stats</title>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="../lib/bootstrap/dist/css/bootstrap.min.css" />
+  <link rel="stylesheet" type="text/css" href="../css/keen-dashboards.css" />
+  <!-- Dataviz dependencies -->
+  <link href="https://d26b395fwzu5fz.cloudfront.net/keen-dataviz-1.0.4.css" rel="stylesheet" />
+  <link href="../css/stats.css" rel="stylesheet" />
 </head>
-<body>
-    <!--[if lt IE 7]>
-        <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-    <![endif]-->
-    <h1>Blank Slate Public Stats</h1>
+<body class="application">
 
-    <!-- Options to change information displayed -->
+  <div id="app-toolbar">
+    <form action="" onsubmit="return false;" method="post">
+      <div class="row tools">
+        <div class="col-sm-2">
+          <div class="tool coordinates">
+            <h5>Model</h5>
+            <select class="form-control" id="models">
+            </select>
+          </div>
+        </div>
+        <div class="col-sm-2">
+          <div class="tool coordinates">
+            <h5>Time Type</h5>
+            <select class="form-control" id="time-type">
+              <option value="created">Created</option>
+              <option value="updated">Updated</option>
+            </select>
+          </div>
+        </div>
+        <div class="col-sm-2">
+          <div class="tool coordinates">
+            <h5>Group By</h5>
+            <select class="form-control" id="group-by">
+              <option value="m/d/y">Day</option>
+              <option value="m/y">Month</option>
+            </select>
+          </div>
+        </div>
+        <div class="col-sm-2">
+          <div class="tool timeframe">
+            <h5>Start date</h5>
+            <input type="date" id="timeframe-start" class="form-control" placeholder="mm/dd/yyyy">
+          </div>
+        </div>
+        <div class="col-sm-2">
+          <div class="tool timeframe">
+            <h5>End date</h5>
+            <input type="date" id="timeframe-end" class="form-control" placeholder="mm/dd/yyyy">
+          </div>
+        </div>
+        <div class="col-sm-2">
+          <div class="tool">
+            <h5>Update Data</h5>
+            <button id="refresh" class="btn btn-primary btn-block">Refresh</button>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
 
-    <div class="models options"><p>Models</p><ul></ul></div>
-    <div class="time_type options">
-      <p>Time Type</p>
-      <ul>
-        <li><button id="created">created</button></li>
-        <li><button id="updated">updated</button></li>
-      </ul>
+  <div class="container-fluid">
+    <div class="row">
+
+      <div class="col-sm-12">
+        <div class="chart-wrapper">
+          <div class="chart-title">
+            Model Stats
+          </div>
+          <div class="chart-stage">
+            <div id="main-chart"></div>
+          </div>
+          <!-- <div class="chart-notes">
+            This is a sample text region to describe this chart.
+          </div> -->
+        </div>
+      </div>
     </div>
-    <div class="time_range options">
-      <p>Time Range - Last...</p>
-      <ul>
-        <li><button id="week">week</button></li>
-        <li><button id="month">month</button></li>
-        <li><button id="forever">forever</button></li>
-      </ul>
+    <!-- <div class="row">
+      <div class="col-sm-4">
+        <div class="chart-wrapper">
+          <div class="chart-title">
+            Today vs. Total
+          </div>
+          <div class="chart-stage">
+            <div class="row">
+              <div class="col-sm-6">
+                <div class="chart-title knob-title">
+                  Total Today
+                </div>
+                <div class="chart-stage" id="metric-01">
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="chart-title knob-title">
+                  Total in Range
+                </div>
+                <div class="chart-stage" id="metric-02">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="chart-notes">
+          </div>
+        </div>
+      </div>
+
+    </div> -->
+
+<div class="row">
+
+      <div class="col-sm-4">
+        <div class="chart-wrapper">
+          <!-- <div class="chart-title">
+            Total Today
+          </div> -->
+          <div class="chart-stage">
+            <div id="metric-01"></div>
+          </div>
+          <!-- <div class="chart-notes">
+            Notes go down here
+          </div> -->
+        </div>
+      </div>
+
+      <div class="col-sm-4">
+        <div class="chart-wrapper">
+          <!-- <div class="chart-title">
+            Other Chart
+          </div> -->
+          <div class="chart-stage">
+            <div id="metric-02"></div>
+          </div>
+          <!-- <div class="chart-notes">
+            Notes go down here
+          </div> -->
+        </div>
+      </div>
+
+      <div class="col-sm-4">
+        <div class="chart-wrapper">
+          <!-- <div class="chart-title">
+            Other Chart
+          </div> -->
+          <div class="chart-stage">
+            <div id="metric-03"></div>
+          </div>
+          <!-- <div class="chart-notes">
+            Notes go down here
+          </div> -->
+        </div>
+      </div>
+
     </div>
-    <div class="date_range options">
-      <p>Date Range</p>
-      <input type="text" id="from" class="date_pick" placeholder="From" />
-      <input type="text" id="to" class="date_pick" placeholder="To" />
-      <button id="get_range">Go</button>
+
+    <!-- <div class="row">
+      <div class="col-sm-3">
+        <div class="chart-wrapper">
+          <img data-src="holder.js/100%x150/white">
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="chart-wrapper">
+          <img data-src="holder.js/100%x150/white">
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="chart-wrapper">
+          <img data-src="holder.js/100%x150/white">
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="chart-wrapper">
+          <img data-src="holder.js/100%x150/white">
+        </div>
+      </div>
     </div>
-    <div class="group_by options">
-      <p>Group By</p>
-      <ul>
-        <li><button id="group_day">day</button></li>
-        <li><button id="group_month">month</button></li>
-      </ul>
-    </div>
 
-    <!-- Summary of information displayed -->
+    <hr>
 
-    <div class="summary">
-      <p class="today">Total today: <span class="today_count"></span></p>
-      <p class="total">Total in range: <span class="total_count"></span></p>
-    </div>
+  </div> -->
 
-    <!-- Div for the SVGs for the graph(s) -->
-    <div class="svgs"></div>
-
-    <script src="../js/vendor/jquery-1.11.1.min.js"></script>
-    <script src="../js/d3.min.js"></script>
-    <script src="../js/plottable.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/qtip2/2.2.1/jquery.qtip.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/jquery.ui/1.9.1/themes/base/minified/jquery-ui.min.css" />
-    <script src="https://cdn.jsdelivr.net/jquery.ui/1.9.1/jquery-ui.min.js"></script>
-    <script src="../js/stats.js"></script>
-<!--
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-60041451-1', 'auto');
-      ga('send', 'pageview');
-
-    </script>
--->
-
+  <script type="text/javascript" src="../js/vendor/jquery-1.11.1.min.js"></script>
+  <script type="text/javascript" src="..//lib/bootstrap/dist/js/bootstrap.min.js"></script>
+  <script src="https://d26b395fwzu5fz.cloudfront.net/keen-analysis-1.1.0.js"></script>
+  <script src="https://d26b395fwzu5fz.cloudfront.net/keen-dataviz-1.0.4.js"></script>
+  <script type="text/javascript" src="../js/stats.js?v=refresh1"></script>
 </body>
 </html>
